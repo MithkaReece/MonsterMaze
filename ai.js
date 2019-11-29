43c5b339664be855f268bbb071c1bf0e60598b72
MithkaReece <reece.waite1@btinternet.com>
2019-11-29 19:33:30 +0000

    Tweaks and comments

--- ai.js
+++ ai.js
@@ -7,12 +7,11 @@
 class monster extends entity{
     constructor(pos){
         super(pos);
-        this.nextPos = pos.copy()
-
-        this.relayMemory = new Queue(6);//QUEUE
+        this.nextPos = pos.copy()//Defines nextPos as a copy of the given position
+        this.distAway;//Distance from player
+        this.relayMemory = new Queue(6);//Defines the relayMemory as a queue of memories size 6
         
-        this.sampleFraction = 0.7;//Fraction of samples that is tested as a batch
-
+        this.Cuboid = new cuboid(createVector(Math.floor(pos.x)+0.25,Math.floor(pos.y),Math.floor(pos.z)+0.25),0.5,1,0.5,0,[255,20,20]);
         this.policyNetwork = new neuralNetwork([6,10,8,4]);//Creates the policy network
         this.targetNetwork = new neuralNetwork([6,10,8,4]);//Create the target network the same size as target network
         this.updateTargetNetwork();//Updates target network to reflect the policy network
@@ -22,19 +21,10 @@ class monster extends entity{
         this.minExploration = 0.1;//Define minExploration as the minimum probability of the ai choosing to explore randomly
         this.explorationDecay = 0.005;//Define explorationDecay as the amount the exploreThreshold decreasing each run through
 
-        this.state; //Vector of monster to position and four surrounding walls
-
-        this.speed = 10;//Calculated based on how far it will move compared to how long it makes decisions (distance/time)
-
-        this.iterations = 0;//Define the number of iterations since copying the policy network to the target network
-
-        this.distAway;//Distance from player
-
+        this.sampleFraction = 0.7;//Fraction of samples that is tested as a batch
         this.moveIterations = 0;//Define the number of iterations since the ai has made a decision while the ai is moving
-
-
-        this.tempCube = new cuboid(createVector(Math.floor(pos.x)+0.25,Math.floor(pos.y),Math.floor(pos.z)+0.25),0.5,1,0.5,0,[255,20,20]);
-
+        this.targetNIterations = 0;//Define the number of iterations since copying the policy network to the target network
+        this.speed = 10;//Calculated based on how far it will move compared to how long it makes decisions (distance/time)
     }
 
     getValue(){//Get property for the distance of the monster to the player used for sorting
@@ -48,8 +38,7 @@ class monster extends entity{
     run(mazeGrid,playerPos){
         //console.log(p5.Vector.dist(playerPos,this.pos))
         if(this.moveIterations == 0){//If ai has finished moving make next decision (if moveIterations is 0)
-            //Select action (explore/exploit) 
-            let actionIndex = this.selectAction(playerPos,mazeGrid);//Picks an action   
+            let actionIndex = this.selectAction(playerPos,mazeGrid);//Picks an action either from exploring or exploiting from information it has learnt 
             let reward = this.calcReward(mazeGrid,playerPos,actionIndex);//Calculates the reward for picked action
             
             let currentState = this.getState(playerPos,this.pos,mazeGrid);//Collect current state
@@ -59,8 +48,8 @@ class monster extends entity{
             for(let i=0;i<sample.length;i++){//Loops through the sample
                 let data = sample[i];//Set data to current data in sample
                 this.policyNetwork.train(data,this.targetNetwork);//Train the policy network using current data and target network
-                this.iterations++;//Increment iteration counter
-                if(this.iterations % this.TNInterval == 0){//Every TNInterval iterations
+                this.targetNIterations++;//Increment iteration counter
+                if(this.targetNIterations % this.TNInterval == 0){//Every TNInterval iterations
                     this.updateTargetNetwork();//Update the target network to match current policy network
                 }
             }
@@ -82,7 +71,7 @@ class monster extends entity{
             dirVector.setMag(1/this.speed);//Sets the magnitude of the direction vector based on the stored speed
         }       
         this.pos.add(dirVector);//Adds the direction vector to move the monster
-        this.tempCube.setPos(p5.Vector.add(dirVector,this.tempCube.getPos()))//Add direction vector to monster's current position
+        this.Cuboid.setPos(p5.Vector.add(dirVector,this.Cuboid.getPos()))//Add direction vector to monster's current position
     }
     //getState is responsible for converting given player and monster position as well as the maze data into an array that
     //represented the current state of the game that can be used an input to the monster's neural network.
@@ -98,7 +87,7 @@ class monster extends entity{
     //move or a move generated from the monster's neural network. The probabilty for either on to be chosen is variable so gradually changes.
     //When picked by the neural network the output is q-values for each possible move and the monster attempts the action with the biggest q-value
     selectAction(playerPos,mazeGrid){
-        let actionIndex;
+        let actionIndex;//Defines actionIndex
         let directions = ["N","E","S","W"]//Purely for testing
         if(random(1)>this.exploreThreshold){//If exploitation is needed
             let outputs = matrixToArray(this.policyNetwork.feedForward(this.getState(playerPos, this.pos, mazeGrid))[0]);//Find q-values through neural network
@@ -126,7 +115,6 @@ class monster extends entity{
     //Finally the final reward result is return from this function.
     calcReward(mazeGrid,playerPos,actionIndex){
         let actions = [createVector(0,0,-1),createVector(1,0,0),createVector(0,0,1),createVector(-1,0,0)];//NESW  
-        let directions = ["N","E","S","W"]
         let reward = 0;//Set reward to 0 by default
         let currentCell = mazeGrid[Math.floor(this.pos.x)][Math.floor(this.pos.z)];//find current cell monster is in
         if(currentCell.getWalls()[actionIndex]==1){//If you walk into a wall
@@ -145,7 +133,7 @@ class monster extends entity{
     //return what memory is there.
     getSample(){
         if(this.relayMemory.getCurrentLength()>Math.floor(this.sampleFraction*this.relayMemory.getLength())){//If the sample size is larger than the fraction needed
-            let newSample = [];
+            let newSample = [];//Define new sample as an empty array
             let memoryData = this.relayMemory.getData();//Copy the relay memory data
             while (newSample.length<Math.floor(this.sampleFraction*this.relayMemory.getLength())){//While newSample length is too small
                 let i = Math.floor(Math.random(0,memoryData.length))//Randomly pick a memory from temp (relay memory copy)
@@ -162,17 +150,17 @@ class monster extends entity{
     //whilst move the policy network away in the same direction which is why there is a target network in the first place.
     updateTargetNetwork(){
         let weights = this.policyNetwork.getWeights();
-        let copiedWeights = [];
-        for(let i=0;i<weights.length;i++){
-            let current = weights[i];
-            let data = current.getData();
-            copiedWeights.push(new Matrix(data));
+        let copiedWeights = [];//Defines an empty list for the weights to be copied in
+        for(let i=0;i<weights.length;i++){//Loop through each layer of weights
+            let current = weights[i];//Set current to the weights between the current layer and the next
+            let data = current.getData();//Set data to the matrix data of the current weights
+            copiedWeights.push(new Matrix(data));//Add a copy of the matrix to copiedWeights list
         }
-        this.targetNetwork.setWeights(copiedWeights);
+        this.targetNetwork.setWeights(copiedWeights);//Sete the weights of the targetNetwork to the copied weights from policy network
     }
     //show3D is responsible for showing the monster is 3D.
     show3D(player,perspective){
-        this.tempCube.show3D(player,perspective)
+        this.Cuboid.show3D(player,perspective)//Call show3D on the monster's cuboid
     }
 }
 //neuralNetwork is a class responsible for storing all the layers of a neural network as a list of matrices
@@ -182,34 +170,34 @@ class monster extends entity{
 //errors of the outputs by back propagating through the layers and tweaking weights using gradient descent
 class neuralNetwork{
     constructor(layers){
-        this.inputCount = layers[0];
-        this.layers = layers;
-        this.weights = this.setupWeights(layers);
-        this.initialiseWeights();
-        this.learningRate = 0.1;
-        this.discountFactor = 0.7;
-        this.activationFunction = this.sigmoid;
-        this.derivActivationFunction = ((x) =>{return this.sigmoid(x)*(1-this.sigmoid(x))});
+        this.inputCount = layers[0];//Define the number of inputs for the network
+        this.layers = layers;//Define layers as a list of number of nodes in each layer
+        this.weights = this.setupWeights(layers);//Define weights as a list of matrices with the corresponding sizes to the layers
+        this.initialiseWeights();//Initialise all the weights with random values
+        this.learningRate = 0.1;//Define the learning rate as 0.1
+        this.discountFactor = 0.7;//Define the discountFactor of future rewards as 0.7
+        this.activationFunction = this.sigmoid;//Define the activationFunction as the sigmoid function
+        this.derivActivationFunction = ((x) =>{return this.sigmoid(x)*(1-this.sigmoid(x))});//Define the derivative of the activation function as the derivative of the sigmoid function
     }
     //sigmoid function is a mathematical function being used as an activation function as it has a useful range of values.
     sigmoid = (x) => {1/(1 + Math.pow(Math.E,-x))}
     //setupWeights is responsible for create an array of matrices which are the correct size for the weights between layers.
     setupWeights(layers){
-        let weights = [];
+        let weights = [];//Set weights to empty array for weights to go in
         for(let i=0;i<layers.length-1;i++){//Loop through all connections between layers
-            let a = layers[i+1];
-            let b = layers[i];
+            let a = layers[i+1];//Set a to the next layer
+            let b = layers[i];//Set b to the current layer
             weights.push(new Matrix(a,b));//Form a matrix representing weights between two layers
         }
-        return weights;
+        return weights;//Return new weights array
     }
     //initialiseWeights is responsible for filling the weights of random initial values.
     initialiseWeights(){
-        for(let i=0;i<this.weights.length;i++){
-            let current = this.weights[i];//Current connection between two layers
-            for(let cols=0;cols<current.cols;cols++){
-                for(let rows=0;rows<current.rows;rows++){
-                    current.data[rows][cols] = Math.random(-1,1);
+        for(let i=0;i<this.weights.length;i++){//For every layer of the network
+            let current = this.weights[i];//Set current to current weights connecting between the two current layers
+            for(let cols=0;cols<current.cols;cols++){//For every column of current weights
+                for(let rows=0;rows<current.rows;rows++){//For every row of current weights
+                    current.data[rows][cols] = Math.random(-1,1);//Set element to a random number between -1 and 1
                 }
             }
         }
@@ -223,10 +211,10 @@ class neuralNetwork{
     //train is responsible for running through the procedure of calculating the error of the neural network and
     //therefore which way to adjust the network and then using gradient descent to adjust towards reducing the error;
     train(data,targetNetwork){
-        let state = clone(data[0]);//Input state
-        let actionIndex = data[1];
-        let reward = data[2];
-        let newState = clone(data[3]);
+        let state = clone(data[0]);//start state from given data
+        let actionIndex = data[1];//actionIndex from given data
+        let reward = data[2];//reward from given data
+        let newState = clone(data[3]);//new start from given data
 
         let outputs = this.feedForward(state);//Gets list of outputs throughout the neural network
         let finalOutput = matrixToArray(outputs[0])//Gets the last outputs of the neural network (q values)
@@ -240,47 +228,33 @@ class neuralNetwork{
                 futureQvalue = futureOutputs[i];//Override max if higher max
             }
         }
-
-        let loss = (reward + this.discountFactor*futureQvalue) - Qvalue;//Calc loss
-        //console.log(Math.round(loss*100));
+        let loss = (reward + this.discountFactor*futureQvalue) - Qvalue;//Calculate loss of Qvalue from network and the optimum Qvalue
         let arrayError = new Array(finalOutput.length).fill(0);//Make an array to place loss in
-        arrayError[actionIndex] = loss;//Add in loss
-        let matrixError = new Matrix(arrayError);//Convert to matrix
-
+        arrayError[actionIndex] = loss;//Add in loss to arrayError
+        let matrixError = new Matrix(arrayError);//Convert arrayError to matrix
         this.gradientDecent(matrixError,outputs);//Use gradient descent to update weights to reduce
-        //onsole.log(this.weights[0].getData()[0]);
-
-        //To calculate loss feedforward with new state
-        //From feeding forward new state save the highest q-value
-        //Use highest q-value in loss equation 
-
-        //Call gradient decent on the network
     }
     //feedForward is responsible for running given inputs through the neural network by multiplying
     //all the weight matrices in order to the given inputs to produce the outputs of the neural network.
     feedForward(inputs){
-        let outputs = [];
+        let outputs = [];//Set up empty array for list of outputs through the network
         if(inputs.length != this.inputCount){//If the amount of inputs is not correction
-            return null;
+            return null;//Return null
         }
         let iMatrix = new Matrix(clone(inputs));//Turn inputs into a matrix
-        outputs.push(new Matrix(clone(iMatrix.getData())));
+        outputs.push(new Matrix(clone(iMatrix.getData())));//Push a copy of the first inputs matrix to list of outputs
         for(let i=0;i<this.weights.length;i++){//Loops through the layers
-            let currentWeights = this.weights[i];
-            iMatrix = Matrix.multiply(currentWeights,iMatrix);//Times by the weights of
-            iMatrix = Matrix.map(iMatrix,this.activationFunction) //<= need to fix so activation function acc works
-            outputs.push(new Matrix(clone(iMatrix.getData())));
+            let currentWeights = this.weights[i];//Set currentWeight to the weights at the current layer
+            iMatrix = Matrix.multiply(currentWeights,iMatrix);//Times the current inputs matrix by the current weights
+            iMatrix = Matrix.map(iMatrix,this.activationFunction)//Maps the result matrix by the activation function
+            outputs.push(new Matrix(clone(iMatrix.getData())));//Push a copy of the current outputs matrix to list of outputs
         }
-        //console.log(outputs)
-        return outputs.reverse();//Reverses the outputs list so that the final outputs are the first element
+        return outputs.reverse();//Return the reverse of the outputs list so that the final outputs are the first element
     }
     //gradientDescent is responsible for backpropogating through the network calculating the errors for each individual
     //nodes of every layer of the neural network and then adjust each weight towards reduces that individual error by
     //gradient descent using the derivative of the activation function.
     gradientDecent(matrixError,outputs){
-        //Updates weights based on loss 
-        //Account learning rate
-        //this.printNetwork()
         let currentErrors = matrixError;//Define the intital output error (q error)
         for(let i=0;i<this.layers.length-1;i++){//Loop through and update all weights
             let currentOutputs = outputs[i]//Sets current outputs
@@ -289,13 +263,11 @@ class neuralNetwork{
             let currentTranspose = Matrix.transpose(this.weights[this.layers.length-i-2]);//Transpose the weights between current layers
             let nextError = Matrix.multiply(currentTranspose,currentErrors);//Calculate the next layers errors
             //New weights = learningRate * currentErrors * (derivative(outputs of layer)) * Transpose(inputs of layer)
-            let lrCurrentErrors = Matrix.scale(currentErrors,this.learningRate);
-            let outputDerivative = Matrix.map(currentOutputs,this.derivActivationFunction);//Map isn't treating it as static
-            let inputTranspose = Matrix.transpose(currentInputs);
-            let deltaWeights = Matrix.multiply(Matrix.elementWiseMult(lrCurrentErrors,outputDerivative),inputTranspose);
-            //console.log(this.weights[i])
-            //console.log(deltaWeights.getData())
-            this.weights[this.layers.length-i-2].add(deltaWeights);//Update weights
+            let lrCurrentErrors = Matrix.scale(currentErrors,this.learningRate);//Calculate the current errors scaled by the learning rate
+            let outputDerivative = Matrix.map(currentOutputs,this.derivActivationFunction);//Calculate the result of applying the derivate of the activation function on to the current outputs
+            let inputTranspose = Matrix.transpose(currentInputs);//Calculate the transpose the current input matrix
+            let deltaWeights = Matrix.multiply(Matrix.elementWiseMult(lrCurrentErrors,outputDerivative),inputTranspose);//Calculates the change in the current layer's weights
+            this.weights[this.layers.length-i-2].add(deltaWeights);//Update current layer's weights
             currentErrors = nextError;//Use the next errors as current for next iteration
         }
     }
